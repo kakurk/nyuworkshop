09030d76e9bf356c3ff666413af583c496c96df6
Kyle Kurkela <kkurkela@bu.edu>
2025-06-16 00:07:54 -0400

    update 2

--- 01_download_project.sh
+++ 01_download_project.sh
@@ -3,7 +3,7 @@
 # load the necessary modules into the enviornment
 
 module load python3/3.10.12
-source venvs/yaxil/bin/activate
+source venvs/nyuworkshop/bin/activate
 
 # run the python script. Depending on the size of the project, this may take a while
 # the script will download the DICOM files and write them into a subdirectory "dicoms"
